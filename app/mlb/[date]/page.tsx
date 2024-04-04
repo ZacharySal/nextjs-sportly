fdc2394d5986284c491edbd45ffa0547429e977b
Zachary <zach.salvaggio@gmail.com>
2024-04-04 02:42:12 -0500

    fix: mlb scoreboard data fetching

--- app/mlb/[date]/page.tsx
+++ app/mlb/[date]/page.tsx
@@ -1,25 +1,5 @@
 import View from "../../../components/MLB/views/Home";
 
-async function getScoreData() {
-  const scoreData = await fetch(
-    `https://cdn.espn.com/core/mlb/scoreboard?xhr=1`,
-    {
-      next: { revalidate: 30 },
-    },
-  );
-
-  if (!scoreData.ok) {
-    throw new Error("Failed to fetch NBA score data");
-  }
-
-  const scoreDataResponse = await scoreData.json();
-
-  return {
-    scoreData: scoreDataResponse,
-  };
-}
-
 export default async function Page({ params }: { params: { date: string } }) {
-  const data = await getScoreData();
-  return <View data={data} date={params.date} />;
+  return <View date={params.date} />;
 }
--- components/MLB/views/Home.tsx
+++ components/MLB/views/Home.tsx
@@ -2,32 +2,41 @@
 
 import LeagueContainerBox from "@/components/LeagueContainerBox";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import useSWR from "swr";
 import Articles from "../../Articles";
 import LeagueUserSelection from "../../LeagueUserSelection";
 import Loading from "../../Loading";
-import useHasHydrated from "../../hooks/useHasHyrdated";
 import MLBScoreboard from "../MLBScoreboard";
 
-export default function Home({ data, date }: { data: any; date?: string }) {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function Home({ date }: { date?: string }) {
   const isDesktopScreen = useMediaQuery("(min-width:800px)");
-  const pageHydrated = useHasHydrated();
+
+  const { data, isLoading } = useSWR(
+    "https://nextjs-sportly.vercel.app/api/leagueData/mlb",
+    fetcher,
+    {
+      refreshInterval: 5000,
+    },
+  );
+
+  if (!isLoading) {
+    console.log(data);
+  }
 
   const desktopView = () => (
     <>
-      <MLBScoreboard initialScoreData={data.scoreData} date={date} />
-      <Articles
-        title={`MLB News`}
-        news={data.scoreData.news.articles}
-        limit={10}
-      />
+      <MLBScoreboard initialScoreData={data} date={date} />
+      <Articles title={`MLB News`} news={data.news.articles} limit={10} />
     </>
   );
 
   const mobileView = () => (
-    <MLBScoreboard initialScoreData={data.scoreData} date={date} />
+    <MLBScoreboard initialScoreData={data} date={date} />
   );
 
-  if (!pageHydrated) return <Loading />;
+  if (isLoading) return <Loading />;
   else
     return (
       <>
